bc55b7909ae462ba8e06e6ed6c4d3b5333173e59
prxsto <15711032+prxsto@users.noreply.github.com>
2024-06-27 16:40:57 -0700

    Remove base styles, create simple scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import "./App.css";
+import { createRoot } from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
 
 function App() {
-  return <></>;
+  return (
+    <div className="canvas-container">
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <mesh>
+          <boxGeometry args={[2, 2, 2]} />
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
+    </div>
+  );
 }
+
+createRoot(document.getElementById("root")).render(<App />);
 
 export default App;
